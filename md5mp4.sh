91dd53316ec4a64b776ec75dc50b91f62c27849c
root <root@enti>
2020-11-27 11:42:38 +0100

    repaso MD5 en MP4

--- md5mp4.sh
+++ md5mp4.sh
@@ -3,6 +3,12 @@
 echo "EJERCICIO MD5 y BUCLES"
 
 for VARIABLE in `ls *.mp4`; do
-	md5sum $VARIABLE | awk '{ print $2 }' | cowsay
-	md5sum $VARIABLE | awk '{ print $1 }'
+	echo "_____________"
+	VIDEO=`file $VARIABLE | grep Media`
+	if [ "$VIDEO" != "" ]; then
+		md5sum $VARIABLE
+	fi	
+		md5sum $VARIABLE | awk '{ print $2 }' | cowsay
+		md5sum $VARIABLE | awk '{ print $1 }' | cowsay
 done
+
