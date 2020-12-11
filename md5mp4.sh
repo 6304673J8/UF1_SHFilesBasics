c3bdebaa50c06d9dd9b23ecbab49ce1d909d64b7
6304673J8 <petergeorghe.julca@enti.cat>
2020-12-11 11:19:25 +0100

    ps learning + VIM cheatsheet

--- md5mp4.sh
+++ md5mp4.sh
@@ -2,17 +2,19 @@
 
 echo "EJERCICIO MD5 y BUCLES"
 
+EXTENSION="mp3"
+
 for FILE in `ls *.mp4`; do
 	echo "_____________"
 	VIDEO=`file $FILE | grep -i Media`
 	echo $VIDEO
 	if [ "$VIDEO" != "" ]; then
 		#md5sum $FILE
+		echo "Convirtiendo $FILE a $EXTENSION"
+		sleep 4
 		NAME=`echo $FILE | cut -d "." -f 1`
 
-		ffmpeg -i $FILE $NAME.mkv 
+		ffmpeg -i $FILE $NAME.$EXTENSION 
+		sleep 2
 	fi	
-
-
 done
-
